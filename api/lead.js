f5671aac33ba8bb43efae44418cbb5dd5afe0eb3
Asbjornhornet <asbjornhornet@gmail.com>
2026-03-23 22:20:49 +0100

    Update lead.js

--- api/lead.js
+++ api/lead.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
-  console.log("LEAD:", req.body);
-  res.status(200).json({ ok: true });
+  console.log("NY LEAD:", req.body);
+  res.json({ok:true});
 }
